7c729723fbfbfbca8ddf6044a32d66e8aa490bdc
Dennis Liaw <dennis.liaw@gmail.com>
2017-12-04 18:26:29 -0500

    completes frag-3

--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -1,12 +1,17 @@
 precision highp float;
 
+#define CIRCLE_COLOR    vec4(1.0, 0.4313, 0.3411, 1.0)
+
 uniform sampler2D texture;
 uniform vec2 screenSize;
 
 void main() {
-  vec2 coord = gl_FragCoord.xy / screenSize;
+  vec2 coord   = gl_FragCoord.xy / screenSize;
+  vec4 texture = texture2D(texture, coord);
 
-  //TODO: Swap red and blue color channels of image
+  float tmp = texture.r;
+  texture.r = texture.b;
+  texture.b = tmp;
 
-  gl_FragColor = texture2D(texture, coord);
+  gl_FragColor = texture;
 }
