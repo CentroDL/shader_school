c55f0a1a9b71b82050526f27f7929c2afb3a2844
Dennis Liaw <dennis.liaw@gmail.com>
2017-12-08 14:37:54 -0500

    solves frag-1 example. TODO: read up on geometric proof for rotation matrix

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -1,12 +1,14 @@
 precision highp float;
 
 uniform float theta;
-
 attribute vec2 position;
 
 void main() {
+  // read up on rotation matrices and figure out the geometry
+  mat4 rotation_matrix = mat4(cos(-theta), -sin(-theta), 0.0, 0.0,
+                  sin(-theta), cos(-theta), 0.0, 0.0,
+                  0.0, 0.0, 1.0, 0.0,
+                  0.0, 0.0, 0.0, 1.0);
 
-  //TODO: rotate position by theta radians about the origin
-
-  gl_Position = vec4(position, 0, 1.0);
+  gl_Position = rotation_matrix * vec4(position, 0.0, 1.0);
 }
